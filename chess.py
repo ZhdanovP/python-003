56b4697e0b8f5826cbbe60687efdc54722140af9
omakukha <omakukha@luxoft.com>
2021-02-15 11:11:42 +0200

    Add test chess file

---
+++ chess.py
@@ -0,0 +1,11 @@
+# Press Shift+F10 to execute it or replace it with your code.
+
+class Chess
+
+def print_hi(name):
+
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+   #  print_hi('PyCharm')
