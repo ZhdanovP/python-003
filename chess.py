27f4aa7d5a5756652c7f5fb36b5986a37e048842
Alexander Nezhurenko <nezhurenko.external@crxmarkets.com>
2021-02-17 01:16:26 +0200

    Abstract classes/slots and modules ver1

--- chess.py
+++ chess.py
@@ -1,20 +1,20 @@
 from figures import Pawn, King, Qeen, Board
-from tests import *
+import tests
 
 """Module simulates chess board with 8 random figures
  and their movement"""
 
 # Execute Some tests
 b1 = Board()
-test_0(b1)
+tests.test_0(b1)
 
 p1 = Pawn(3, 3)
-test_1(p1)
+tests.test_1(p1)
 
 k1 = King(3, 3)
-test_2(k1)
+tests.test_2(k1)
 
 q1 = Qeen(3, 3)
-test_3(q1)
+tests.test_3(q1)
 
 
