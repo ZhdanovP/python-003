facfa512926a50cc8a98c32777ee0c408ec42702
Nataliia Petrova <npetrova@luxoft.com>
2021-02-18 09:21:46 +0200

    A simple decorator has been added

--- chess.py
+++ chess.py
@@ -15,6 +15,26 @@ def setup_empty_board():
     return board
 
 
+def simple_html_decorator(func):
+    """
+    A simple decorator designated for simple test functions with terminal output.
+    Before test function output, it informs that that the test function is about to start.
+    It then wraps test function output into <p> HTML tag.
+    Then it informs that a given test function is ended.
+    :param func: A function whose terminal output needs to be decorated with HTML.
+    :return: A decorated function wrapper.
+    """
+    def wrapper():
+        print("<h1>Starting test function.</h1>")
+        print("<p>")
+        func()
+        print("</p>")
+        print("<h1>Test function finished.</h1>")
+
+    return wrapper
+
+
+@simple_html_decorator
 def test_left_white_knight():
     """A simple test for a left white knight."""
     board = setup_empty_board()
@@ -25,6 +45,7 @@ def test_left_white_knight():
     print("Is knight B1 allowed to move to C3: " + str(is_allowed))
 
 
+@simple_html_decorator
 def test_black_pawn():
     """A simple test for a black pawn."""
     board = setup_empty_board()
